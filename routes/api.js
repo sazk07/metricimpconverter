d86e730a99bd4f311ef85071b13839a7427efe1b
shahan <shahan@gmail.com>
2023-06-10 02:55:09 +0500

    feat: complete api GET route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
+  app.get('/api/convert', (req, res) => {
+    const { input } = req.params
+    const initNum = convertHandler.getNum(input)
+    const initUnit = convertHandler.getUnit(input)
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const stringOutput = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string: stringOutput
+    })
+  })
 
 };
